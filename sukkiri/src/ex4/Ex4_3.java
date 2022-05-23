f878ff79e0922eef84b348501b2129fcf1cba22e
SeiichiN <billie175@gmail.com>
2022-05-24 08:12:05 +0900

    ex4 modified.

--- sukkiri/src/ex4/Ex4_3.java
+++ sukkiri/src/ex4/Ex4_3.java
@@ -3,6 +3,7 @@ package ex4;
 public class Ex4_3 {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("unused")
 		int[] counts = null;
 		float[] heights = {171.3F, 175.0F};
 		// System.out.println(counts[1]);
--- sukkiri/src/ex4/Ex4_4.java
+++ sukkiri/src/ex4/Ex4_4.java
@@ -1,5 +1,9 @@
 package ex4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Ex4_4 {
 
 	public static void main(String[] args) {
@@ -14,8 +18,24 @@ public class Ex4_4 {
 		System.out.println("1けたの数字を入力してください");
 		
 		// (3)
-		int input = new java.util.Scanner(System.in).nextInt();
-		
+		int input = 0;
+		Scanner in = null;
+		try {
+			in = new java.util.Scanner(new File("ex4.txt"));
+			input = in.nextInt();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}
 		// (4)
 		for (int num : numbers) {
 			if (input == num) {
